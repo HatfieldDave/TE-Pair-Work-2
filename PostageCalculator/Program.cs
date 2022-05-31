bf5490f126c3f630dfaccbff08765043db7466d2
Matt Eland <matt.eland@gmail.com>
2022-05-31 14:39:31 -0400

    Moved to .NET Core 3.1; made classes public

--- PostageCalculator/Program.cs
+++ PostageCalculator/Program.cs
@@ -2,7 +2,7 @@
 
 namespace PostageCalculator
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
--- TollBoothCalculator/Program.cs
+++ TollBoothCalculator/Program.cs
@@ -2,7 +2,7 @@
 
 namespace TollBoothCalculator
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
